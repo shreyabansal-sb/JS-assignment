f304116a13a084ad7ee71ea5e5a0b2e55e5905fb
Shreya Bansal <253486207+shreyabansal-sb@users.noreply.github.com>
2026-04-14 21:45:54 +0530

    remved html file and switched it for js
    Count numbers divisible by K in the range [L, R] with non-zero digits whose digit sum is prime.

--- q1.js
+++ q1.js
@@ -1,1 +1,47 @@
+let L = Number(prompt("Enter L:"));
+let R = Number(prompt("Enter R:"));
+let K = Number(prompt("Enter K:"));
 
+let count = 0;
+
+for (let x= L; x <= R; x++) {
+  
+  if (x%K != 0) 
+    continue;
+
+  let temp= x;
+  let sum= 0;
+  let hasZero= 0;
+
+  while (temp > 0) {
+    
+    let digit= temp%10;
+    
+    if (digit ==0) {
+      hasZero=1;
+      break;
+    }
+    
+    sum += digit;
+    temp = (temp-digit)/10;
+  }
+  
+  if (hasZero ==1) 
+    continue;
+  
+  let isPrime = 1;
+  
+  if (sum < 2) 
+    isPrime = 0;
+  
+  for (let i = 2; i < sum; i++) {
+    if (sum % i == 0) {
+      isPrime = 0;
+      break;
+    }
+  }
+  if (isPrime == 1) 
+    count++;
+}
+
+alert(count);  
