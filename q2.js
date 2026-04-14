35bf0e329de9306d9271f5bb6c494dfc0accff26
Shreya Bansal <253486207+shreyabansal-sb@users.noreply.github.com>
2026-04-14 21:51:56 +0530

    removed q2html files for q2js

--- q2.js
+++ q2.js
@@ -1,1 +1,26 @@
+let N = Number(prompt("Enter N:"));
+let seed = Number(prompt("Enter seed:"));
 
+let current = N;
+for (let i=0; i<3; i++) {
+
+  if (current%2 === 0) {
+    current = current/2 + seed;
+  }
+  else {
+    current = current*3 - seed;
+  }
+}
+
+let ans="NO";
+
+if (current>=100 && current<=999){
+  let rem=current%10;
+  let temp = (current - rem) /10;
+  let middigit = temp%10;
+  if (middigit === seed){
+    ans="YES";
+  }
+}
+
+alert(ans + ", " + current);
